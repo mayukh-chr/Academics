308b64cea34f61003cb29ad109e4f50bb906eeff
Mayukh Saha <106549654+mayukh-chr@users.noreply.github.com>
2024-01-23 15:22:28 +0530

    Update 3.1.c

--- DAA lab/3.1.c
+++ DAA lab/3.1.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 
-
+unsigned int opcount= 0;
 void bubblesort(int arr[], int n){
     int i ,j, temp;
     for(i=0;i<n-1; i++){
         for(j=0;j<n-i-1;j++){
            if(arr[j] > arr[j+1]){
+                opcount++;
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
@@ -14,20 +17,28 @@ void bubblesort(int arr[], int n){
     }
 }
 
+
+
 int main(){
-    int n;
+    long long int n;
+
+    clock_t start, endl;
+  double cpu_time;
     printf("Enter size of array: ");
     scanf("%d", &n);
-
+    srand(time(NULL));
     int arr1[n];
     printf("Enter elements: \n");
     for(int i=0; i<n; i++)
-        scanf("%d", &arr1[i]);
-
+        arr1[i] = (rand()%1000) +1;
+    start = clock();
     bubblesort(arr1, n);
-
+    endl = clock();
+    cpu_time = (double)(endl-start)/CLOCKS_PER_SEC;
     for(int i=0; i<n; i++)
         printf("%d ", arr1[i]);
+
+    printf("%e    %d", cpu_time, opcount);
 
     return 0;
 }
