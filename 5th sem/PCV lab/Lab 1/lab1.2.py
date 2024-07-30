0c7cdbd284154cca6083b9ecae59be258ce2472f
Mayukh Saha <106549654+mayukh-chr@users.noreply.github.com>
2024-07-30 11:06:33 +0530

    Add files via upload

--- 5th sem/PCV lab/Lab 1/lab1.2.py
+++ 5th sem/PCV lab/Lab 1/lab1.2.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 img = cv2.imread("C:/Users/Student/Documents/90198/CV lab/image.jpg")
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
@@ -8,6 +9,24 @@ neg = cv2.bitwise_not(gray)
 # cv2.imshow('Input Image', img)
 # cv2.imshow('Grey image', gray)  
 # cv2.imshow('Negative Image', neg) 
+
+# img = cv.imread('gradient.png', cv.IMREAD_GRAYSCALE)
+# assert img is not None, "file could not be read, check with os.path.exists()"
+# ret,thresh1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+# ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
+# ret,thresh3 = cv.threshold(img,127,255,cv.THRESH_TRUNC)
+# ret,thresh4 = cv.threshold(img,127,255,cv.THRESH_TOZERO)
+# ret,thresh5 = cv.threshold(img,127,255,cv.THRESH_TOZERO_INV)
+ 
+# titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+# images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+ 
+# for i in range(6):
+#     plt.subplot(2,3,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
+#     plt.title(titles[i])
+#     plt.xticks([]),plt.yticks([])
+ 
+# plt.show()
 height, width = img.shape[:2]
 gheight, gwidth = gray.shape[:2]
 
@@ -17,14 +36,6 @@ y = input("input x coordinate: ")
 
 blue, green, red  = img[x,y,0], img[x,y,1], img[x,y,2]
 
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
-ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
-ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
-ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
- 
-titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
 print(img.shape)
 print(gray.shape)
 
--- 5th sem/PCV lab/Lab 1/lab1.3.py
+++ 5th sem/PCV lab/Lab 1/lab1.3.py
@@ -20,17 +20,7 @@ while True:
 	key = cv2.waitKey(1) 
 	if key == ord("q"): 
 		break
-	print("CV_CAP_PROP_FRAME_WIDTH: '{}'".format(source.get(cv2.CAP_PROP_FRAME_WIDTH))) 
-print("CV_CAP_PROP_FRAME_HEIGHT : '{}'".format(source.get(cv2.CAP_PROP_FRAME_HEIGHT))) 
-print("CAP_PROP_FPS : '{}'".format(source.get(cv2.CAP_PROP_FPS))) 
-print("CAP_PROP_POS_MSEC : '{}'".format(source.get(cv2.CAP_PROP_POS_MSEC))) 
-print("CAP_PROP_FRAME_COUNT  : '{}'".format(source.get(cv2.CAP_PROP_FRAME_COUNT))) 
-print("CAP_PROP_BRIGHTNESS : '{}'".format(source.get(cv2.CAP_PROP_BRIGHTNESS))) 
-print("CAP_PROP_CONTRAST : '{}'".format(source.get(cv2.CAP_PROP_CONTRAST))) 
-print("CAP_PROP_SATURATION : '{}'".format(source.get(cv2.CAP_PROP_SATURATION))) 
-print("CAP_PROP_HUE : '{}'".format(source.get(cv2.CAP_PROP_HUE))) 
-print("CAP_PROP_GAIN  : '{}'".format(source.get(cv2.CAP_PROP_GAIN))) 
-print("CAP_PROP_CONVERT_RGB : '{}'".format(source.get(cv2.CAP_PROP_CONVERT_RGB))) 
+	
 # closing the window 
 cv2.destroyAllWindows() 
 source.release()
